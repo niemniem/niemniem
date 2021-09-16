0781069b0cbf2e4c825617edac785cf1c2564271
Niem <niem.harris@wpengine.com>
2021-09-15 20:28:22 -0500

    Saitama

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="Yay *111111"
+          title="Yay *1177111"
           buttonText="Developer Docs"
           buttonURL="https://developers.wpengine.com/"
           button2Text="Headless on GitHub"
